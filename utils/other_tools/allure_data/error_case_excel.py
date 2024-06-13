4c06a0683d6196922611149e298fffa8ac704b3f
Cassianvale <1544257291@qq.com>
2024-06-13 17:49:29 +0800

    fix: 修复收集异常用例EXCEL

--- utils/other_tools/allure_data/error_case_excel.py
+++ utils/other_tools/allure_data/error_case_excel.py
@@ -1,20 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 
 import json
 import shutil
 import ast
-import xlwings
+import xlwings as xw
 from common.setting import ensure_path_sep
 from utils.read_files_tools.get_all_files_path import get_all_files
 from utils.notify.wechat_send import WeChatSend
 from utils.other_tools.allure_data.allure_report_data import AllureFileClean
 
 
-# TODO 还需要处理动态值
 class ErrorTestCase:
     """ 收集错误的excel """
+
     def __init__(self):
         self.test_case_path = ensure_path_sep("\\report\\html\\data\\test-cases\\")
 
@@ -50,7 +49,7 @@ class ErrorTestCase:
         用于兼容用例执行异常，未发送请求导致的情况
         @return:
         """
-        parameters = test_case['parameters'][0]['value']
+        parameters = test_case.get('parameters', [{}])[0].get('value', '{}')
         return ast.literal_eval(parameters)
 
     @classmethod
@@ -59,8 +58,7 @@ class ErrorTestCase:
         获取allure报告中请求后的数据
         @return:
         """
-        test_stage = test_case['testStage']['steps']
-        return test_stage
+        return test_case.get('testStage', {}).get('steps', [])
 
     def get_case_url(self, test_case):
         """
@@ -68,13 +66,15 @@ class ErrorTestCase:
         @param test_case:
         @return:
         """
+        test_stage = test_case.get('testStage', {})
         # 判断用例步骤中的数据是否异常
-        if test_case['testStage']['status'] == 'broken':
+        if test_stage.get('status') == 'broken':
             # 如果异常状态下，则获取请求前的数据
-            _url = self.get_parameters(test_case)['url']
+            _url = self.get_parameters(test_case).get('url', 'URL not found')
         else:
-            # 否则拿请求步骤的数据，因为如果设计到依赖，会获取多组，因此我们只取最后一组数据内容
-            _url = self.get_test_stage(test_case)[-7]['name'][7:]
+            # 否则拿请求步骤的数据，因为如果涉及到依赖，会获取多组，因此我们只取最后一组数据内容
+            steps = test_stage.get('steps', [])
+            _url = steps[-7]['name'][7:] if len(steps) > 6 else 'URL not found'
         return _url
 
     def get_method(self, test_case):
@@ -83,10 +83,12 @@ class ErrorTestCase:
         @param test_case:
         @return:
         """
-        if test_case['testStage']['status'] == 'broken':
-            _method = self.get_parameters(test_case)['method']
+        test_stage = test_case.get('testStage', {})
+        if test_stage.get('status') == 'broken':
+            _method = self.get_parameters(test_case).get('method', 'Method not found')
         else:
-            _method = self.get_test_stage(test_case)[-6]['name'][6:]
+            steps = test_stage.get('steps', [])
+            _method = steps[-6]['name'][6:] if len(steps) > 5 else 'Method not found'
         return _method
 
     def get_headers(self, test_case):
@@ -94,37 +96,44 @@ class ErrorTestCase:
         获取用例中的请求头
         @return:
         """
-        if test_case['testStage']['status'] == 'broken':
-            _headers = self.get_parameters(test_case)['headers']
+        test_stage = test_case.get('testStage', {})
+        if test_stage.get('status') == 'broken':
+            _headers = self.get_parameters(test_case).get('headers', 'Headers not found')
         else:
             # 如果用例请求成功，则从allure附件中获取请求头部信息
-            _headers_attachment = self.get_test_stage(test_case)[-5]['attachments'][0]['source']
-            path = ensure_path_sep("\\report\\html\\data\\attachments\\" + _headers_attachment)
-            with open(path, 'r', encoding='utf-8') as file:
-                _headers = json.load(file)
+            steps = test_stage.get('steps', [])
+            if len(steps) > 4 and 'attachments' in steps[-5]:
+                _headers_attachment = steps[-5]['attachments'][0]['source']
+                path = ensure_path_sep("\\report\\html\\data\\attachments\\" + _headers_attachment)
+                try:
+                    with open(path, 'r', encoding='utf-8') as file:
+                        _headers = json.load(file)
+                except FileNotFoundError:
+                    _headers = 'Headers file not found'
+            else:
+                _headers = 'Headers not found'
         return _headers
-
-    def get_request_type(self, test_case):
-        """
-        获取用例的请求类型
-        @param test_case:
-        @return:
-        """
-        request_type = self.get_parameters(test_case)['requestType']
-        return request_type
 
     def get_case_data(self, test_case):
         """
         获取用例内容
         @return:
         """
-        if test_case['testStage']['status'] == 'broken':
-            _case_data = self.get_parameters(test_case)['data']
+        test_stage = test_case.get('testStage', {})
+        if test_stage.get('status') == 'broken':
+            _case_data = self.get_parameters(test_case).get('data', 'Data not found')
         else:
-            _case_data_attachments = self.get_test_stage(test_case)[-4]['attachments'][0]['source']
-            path = ensure_path_sep("\\report\\html\\data\\attachments\\" + _case_data_attachments)
-            with open(path, 'r', encoding='utf-8') as file:
-                _case_data = json.load(file)
+            steps = test_stage.get('steps', [])
+            if len(steps) > 3 and 'attachments' in steps[-4]:
+                _case_data_attachments = steps[-4]['attachments'][0]['source']
+                path = ensure_path_sep("\\report\\html\\data\\attachments\\" + _case_data_attachments)
+                try:
+                    with open(path, 'r', encoding='utf-8') as file:
+                        _case_data = json.load(file)
+                except FileNotFoundError:
+                    _case_data = 'Case data file not found'
+            else:
+                _case_data = 'Data not found'
         return _case_data
 
     def get_dependence_case(self, test_case):
@@ -133,8 +142,7 @@ class ErrorTestCase:
         @param test_case:
         @return:
         """
-        _dependence_case_data = self.get_parameters(test_case)['dependence_case_data']
-        return _dependence_case_data
+        return self.get_parameters(test_case).get('dependence_case_data', 'Dependence case not found')
 
     def get_sql(self, test_case):
         """
@@ -142,8 +150,7 @@ class ErrorTestCase:
         @param test_case:
         @return:
         """
-        sql = self.get_parameters(test_case)['sql']
-        return sql
+        return self.get_parameters(test_case).get('sql', 'SQL not found')
 
     def get_assert(self, test_case):
         """
@@ -151,8 +158,15 @@ class ErrorTestCase:
         @param test_case:
         @return:
         """
-        assert_data = self.get_parameters(test_case)['assert_data']
-        return assert_data
+        return self.get_parameters(test_case).get('assert_data', 'Assert data not found')
+
+    def get_request_type(self, test_case):
+        """
+        获取用例的请求类型
+        @param test_case:
+        @return:
+        """
+        return self.get_parameters(test_case).get('requestType', 'Request type not found')
 
     @classmethod
     def get_response(cls, test_case):
@@ -161,19 +175,22 @@ class ErrorTestCase:
         @param test_case:
         @return:
         """
-        if test_case['testStage']['status'] == 'broken':
-            _res_date = test_case['testStage']['statusMessage']
+        test_stage = test_case.get('testStage', {})
+        if test_stage.get('status') == 'broken':
+            return test_stage.get('statusMessage', 'Response not found')
         else:
+            steps = test_stage.get('steps', [])
+            if not steps:
+                return 'Response not found'
             try:
-                res_data_attachments = \
-                    test_case['testStage']['steps'][-1]['attachments'][0]['source']
+                res_data_attachments = steps[-1].get('attachments', [])[0]['source']
                 path = ensure_path_sep("\\report\\html\\data\\attachments\\" + res_data_attachments)
                 with open(path, 'r', encoding='utf-8') as file:
-                    _res_date = json.load(file)
+                    return json.load(file)
             except FileNotFoundError:
-                # 程序中没有提取到响应数据，返回None
-                _res_date = None
-        return _res_date
+                return 'Response file not found'
+            except (KeyError, IndexError):
+                return 'Response not found'
 
     @classmethod
     def get_case_time(cls, test_case):
@@ -182,9 +199,7 @@ class ErrorTestCase:
         @param test_case:
         @return:
         """
-
-        case_time = str(test_case['time']['duration']) + "ms"
-        return case_time
+        return str(test_case.get('time', {}).get('duration', '0')) + "ms"
 
     @classmethod
     def get_uid(cls, test_case):
@@ -193,100 +208,65 @@ class ErrorTestCase:
         @param test_case:
         @return:
         """
-        uid = test_case['uid']
-        return uid
+        return test_case.get('uid', 'UID not found')
 
 
 class ErrorCaseExcel:
     """ 收集运行失败的用例，整理成excel报告 """
+
     def __init__(self):
         _excel_template = ensure_path_sep("\\utils\\other_tools\\allure_data\\自动化异常测试用例.xlsx")
         self._file_path = ensure_path_sep("\\Files\\test_data\\自动化异常测试用例.xlsx")
-        # if os.path.exists(self._file_path):
-        #     os.remove(self._file_path)
-
         shutil.copyfile(src=_excel_template, dst=self._file_path)
-        # 打开程序（只打开不新建)
-        self.app = xlwings.App(visible=False, add_book=False)
+        self.app = xw.App(visible=False, add_book=False)
         self.w_book = self.app.books.open(self._file_path, read_only=False)
-
-        # 选取工作表：
-        self.sheet = self.w_book.sheets['异常用例']  # 或通过索引选取
+        self.sheet = self.w_book.sheets['异常用例']
         self.case_data = ErrorTestCase()
 
-    def __del__(self):
-        self.w_book.save()
-        self.w_book.close()
-        self.app.quit()
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def close(self):
+        """显式关闭资源的方法"""
+        if self.w_book:
+            self.w_book.save()
+            self.w_book.close()
+        if self.app:
+            self.app.quit()
 
     def background_color(self, position: str, rgb: tuple):
-        """
-        excel 单元格设置背景色
-        @param rgb: rgb 颜色 rgb=(0，255，0)
-        @param position: 位置，如 A1, B1...
-        @return:
-        """
-        # 定位到单元格位置
         rng = self.sheet.range(position)
         excel_rgb = rng.color = rgb
         return excel_rgb
 
     def column_width(self, position: str, width: int):
-        """
-        设置列宽
-        @return:
-        """
         rng = self.sheet.range(position)
-        # 列宽
         excel_column_width = rng.column_width = width
         return excel_column_width
 
     def row_height(self, position, height):
-        """
-        设置行高
-        @param position:
-        @param height:
-        @return:
-        """
         rng = self.sheet.range(position)
         excel_row_height = rng.row_height = height
         return excel_row_height
 
     def column_width_adaptation(self, position):
-        """
-        excel 所有列宽度自适应
-        @return:
-        """
         rng = self.sheet.range(position)
         auto_fit = rng.columns.autofit()
         return auto_fit
 
     def row_width_adaptation(self, position):
-        """
-        excel 设置所有行宽自适应
-        @return:
-        """
         rng = self.sheet.range(position)
         row_adaptation = rng.rows.autofit()
         return row_adaptation
 
     def write_excel_content(self, position: str, value: str):
-        """
-        excel 中写入内容
-        @param value:
-        @param position:
-        @return:
-        """
         self.sheet.range(position).value = value
 
     def write_case(self):
-        """
-        用例中写入失败用例数据
-        @return:
-        """
-
         _data = self.case_data.get_error_case_data()
-        # 判断有数据才进行写入
         if len(_data) > 0:
             num = 2
             for data in _data:
@@ -303,14 +283,11 @@ class ErrorCaseExcel:
                 self.write_excel_content(position="K" + str(num), value=str(self.case_data.get_case_time(data)))
                 self.write_excel_content(position="L" + str(num), value=str(self.case_data.get_response(data)))
                 num += 1
-            self.w_book.save()
-            self.w_book.close()
-            self.app.quit()
-            # 有数据才发送企业微信
+            self.close()
             WeChatSend(AllureFileClean().get_case_count()).send_file_msg(self._file_path)
 
 
-if __name__ == '__main__':
-    em = ErrorCaseExcel()
-    print(em)
-    # ErrorCaseExcel().write_case()
+# 使用上下文管理器的示例
+if __name__ == "__main__":
+    with ErrorCaseExcel() as excel:
+        excel.write_case()
