6f7931558cddeb63318f694b24b7e85eeab93c1c
Cassianvale <1544257291@qq.com>
2024-06-27 17:39:01 +0800

    feat: 以json格式存储cache

--- utils/cache_process/cache_control.py
+++ utils/cache_process/cache_control.py
@@ -1,79 +1,53 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
-"""
-缓存文件处理
-"""
-
 import os
+import json
 from typing import Any, Text, Union
 from common.setting import ensure_path_sep
 from utils.other_tools.exceptions import ValueNotFoundError
 
-
 class Cache:
-    """ 设置、读取缓存 """
-    def __init__(self, filename: Union[Text, None]) -> None:
+    def __init__(self, filename: Union[Text, None] = None) -> None:
+        self.cache_dir = ensure_path_sep("\\cache\\")
+        # 校验cache文件夹
+        if not os.path.exists(self.cache_dir):
+            os.makedirs(self.cache_dir)
         # 如果filename不为空，则操作指定文件内容
         if filename:
-            self.path = ensure_path_sep("\\cache" + filename)
+            self.path = os.path.join(self.cache_dir, f"{filename}.json")
         # 如果filename为None，则操作所有文件内容
         else:
-            self.path = ensure_path_sep("\\cache")
+            self.path = self.cache_dir
 
     def set_cache(self, key: Text, value: Any) -> None:
-        """
-        设置缓存, 只支持设置单字典类型缓存数据, 缓存文件如以存在,则替换之前的缓存内容
-        :return:
-        """
         with open(self.path, 'w', encoding='utf-8') as file:
-            file.write(str({key: value}))
+            json.dump({key: value}, file, indent=4, ensure_ascii=False)
 
     def set_caches(self, value: Any) -> None:
-        """
-        设置多组缓存数据
-        :param value: 缓存内容
-        :return:
-        """
         with open(self.path, 'w', encoding='utf-8') as file:
-            file.write(str(value))
+            json.dump(value, file, indent=4, ensure_ascii=False)
 
     def get_cache(self) -> Any:
-        """
-        获取缓存数据
-        :return:
-        """
         try:
             with open(self.path, 'r', encoding='utf-8') as file:
-                return file.read()
+                return json.load(file)
         except FileNotFoundError:
-            pass
+            return None
 
     def clean_cache(self) -> None:
-        """删除所有缓存文件"""
-
         if not os.path.exists(self.path):
             raise FileNotFoundError(f"您要删除的缓存文件不存在 {self.path}")
         os.remove(self.path)
 
     @classmethod
     def clean_all_cache(cls) -> None:
-        """
-        清除所有缓存文件
-        :return:
-        """
         cache_path = ensure_path_sep("\\cache")
 
         # 列出目录下所有文件，生成一个list
         list_dir = os.listdir(cache_path)
         for i in list_dir:
             # 循环删除文件夹下得所有内容
-            os.remove(cache_path + i)
-
+            os.remove(os.path.join(cache_path, i))
 
 _cache_config = {}
-
 
 class CacheHandler:
     @staticmethod
